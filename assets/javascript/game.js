f30674746b20e5ea58cf089126c5db01f48f674a
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2019-05-05 18:52:30 -0500

    game logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,18 @@
+// Crystal game
+// Requires jquery to run!
+
+// Variable declarations
+var targetNumber = 0;
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
+var gameOver = false;
+
+//Function declarations
+var pickTargetNumber = function() {
+  return Math.floor(Math.random() * 120) + 19;
+}
+
 var growCrystals = function() {
   for (i = 0; i < 4; i++) {
     var crystalButton = $("<button>");
@@ -5,20 +20,67 @@ var growCrystals = function() {
     console.log(scoreValue);
     crystalButton.attr('class' , 'crystal-button');
     crystalButton.attr('score-value' , scoreValue);
-    crystalButton.text('crystal button' , scoreValue);
+    crystalButton.text('button');
     console.log(crystalButton);
     $('#crystals').append(crystalButton);
   }
 }
 
-var pickTargetNumber = function() {
-  return (Math.floor(Math.random()) * 120) + 19;
+var newGame = function() {
+  console.log($('crystals').childElementCount);
+  $('#results').html("");
+  $('#crystals').html("");
+  totalScore = 0;
+  gameOver = false;
+  targetNumber = pickTargetNumber();
+  console.log($('crystals').childElementCount);
+  growCrystals();
+  console.log($('crystals').childElementCount);
+  updateGameboard();
+  console.log(gameOver);
 }
 
-
-$('document').ready(function() {
-  var targetNumber = pickTargetNumber();
+var updateGameboard = function() {
   $('#target-number').text(targetNumber);
-  console.log(targetNumber)
-  growCrystals();
+  $('#total-number').text(totalScore);
+  $('#wins').text(wins);
+  $('#losses').text(losses);
+  console.log(targetNumber, totalScore);
+}
+
+var playerWins = function () {
+  wins++;
+  console.log("win ", wins);
+  updateGameboard();
+  $('#results').text('Player wins! Click any crystal to start a new game!')
+  gameOver = true;
+}
+
+var playerLoses = function () {
+  losses++;
+  console.log("loss ", losses);
+  updateGameboard();
+  $('#results').text('Player loses! Click any crystal to start a new game!')
+  gameOver = true;
+}
+
+// Game
+$('document').ready(function() {
+newGame();
+
+$('#crystals').on('click', '.crystal-button', function() {
+    totalScore += parseInt($(this).attr('score-value'));
+    console.log("pushed ", $(this).attr('score-value'));
+    if (gameOver) {
+      newGame();
+    } else if (totalScore === targetNumber) {
+      // You win
+      playerWins();
+    } else if (totalScore > targetNumber) {
+      // You lose
+      playerLoses();
+    } else {
+      updateGameboard();
+    }
+  })
 });
