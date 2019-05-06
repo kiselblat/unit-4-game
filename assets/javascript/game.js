6d45724894016626a2e7666780dfd33f5eed4e18
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2019-05-05 23:51:33 -0500

    added styling and improved value selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,64 @@
 // Requires jquery to run!
 
 // Variable declarations
+
+// These control the theme of the game
+var buttonList = ["assets/images/Amethyst.jpg" , "assets/images/Emerald.jpg" , "assets/images/Ruby.jpg" , "assets/images/Quartz.jpg"]
+var winMessage = "You've charged the Magic Crystal. Click any minor crystal to charge another!";
+var loseMessage = "You've burned out the Magic Crystal! Click any minor crystal to try again.";
+var inPlayMessage = "Charging the crystal. Careful!"
+// These control how the game works
 var targetNumber = 0;
 var totalScore = 0;
 var wins = 0;
 var losses = 0;
 var gameOver = false;
+var values = []
 
 //Function declarations
+
+//Picks target number, duh
 var pickTargetNumber = function() {
   return Math.floor(Math.random() * 120) + 19;
 }
-
+//Resets and starts a new game
+var newGame = function() {
+  $('#results').text(inPlayMessage);
+  $('#crystals').empty();
+  totalScore = 0;
+  values = [];
+  gameOver = false;
+  targetNumber = pickTargetNumber();
+  growCrystals();
+  updateGameboard();
+  console.log("new game");
+}
+//Picks values for buttons that don't repeat
+var pickValue = function() {
+  for (var i = 0 ; i <= values.length ; i++) {
+    var value = Math.floor(Math.random() * 12) + 1;
+    if (!values.includes(value)) {
+      values.push(value);
+      return value;
+    } else {
+      pickValue();
+    }
+  }
+}
+//Creates the buttons
 var growCrystals = function() {
   for (i = 0; i < 4; i++) {
-    var crystalButton = $("<button>");
-    scoreValue = Math.floor(Math.random() * 12) + 1;
-    console.log(scoreValue);
-    crystalButton.attr('class' , 'crystal-button');
+    var scoreValue = pickValue();
+    var crystalButton = $("<div>");
+    var crystalImage = $('<img>').attr('src' , buttonList[i]);
     crystalButton.attr('score-value' , scoreValue);
-    crystalButton.text('button');
-    console.log(crystalButton);
+    crystalButton.addClass('crystal-button mx-auto d-block');
+    crystalButton.append(crystalImage);
+    console.log(i + 1 , scoreValue);
     $('#crystals').append(crystalButton);
   }
 }
-
-var newGame = function() {
-  console.log($('crystals').childElementCount);
-  $('#results').html("");
-  $('#crystals').html("");
-  totalScore = 0;
-  gameOver = false;
-  targetNumber = pickTargetNumber();
-  console.log($('crystals').childElementCount);
-  growCrystals();
-  console.log($('crystals').childElementCount);
-  updateGameboard();
-  console.log(gameOver);
-}
-
+//Updates the relevent page elements after each click
 var updateGameboard = function() {
   $('#target-number').text(targetNumber);
   $('#total-number').text(totalScore);
@@ -47,40 +67,38 @@ var updateGameboard = function() {
   $('#losses').text(losses);
   console.log(targetNumber, totalScore);
 }
-
+//The win routine
 var playerWins = function () {
   wins++;
-  console.log("win ", wins);
+  console.log("win " , wins);
   updateGameboard();
-  $('#results').text('Player wins! Click any crystal to start a new game!')
-  gameOver = true;
+  $('#results').text(winMessage);
 }
-
+//The lose routine
 var playerLoses = function () {
   losses++;
-  console.log("loss ", losses);
+  console.log("loss " , losses);
   updateGameboard();
-  $('#results').text('Player loses! Click any crystal to start a new game!')
-  gameOver = true;
+  $('#results').text(loseMessage);
 }
 
 // Game
 $('document').ready(function() {
-newGame();
-
-$('#crystals').on('click', '.crystal-button', function() {
+  newGame();
+  //Click handler for div containing the buttons, applied only to buttons
+  $('#crystals').on('click' , '.crystal-button' , function() {
     totalScore += parseInt($(this).attr('score-value'));
-    console.log("pushed ", $(this).attr('score-value'));
+    console.log("pushed " , $(this).attr('score-value'));
     if (gameOver) {
       newGame();
     } else if (totalScore === targetNumber) {
-      // You win
+      gameOver = true;
       playerWins();
     } else if (totalScore > targetNumber) {
-      // You lose
       playerLoses();
+      gameOver = true;
     } else {
-      updateGameboard();
+        updateGameboard();
     }
   })
 });
